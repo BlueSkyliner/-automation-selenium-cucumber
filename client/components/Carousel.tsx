3124401953e3030b1e7a424ef55e55633d17f8d0
BlueSkyliner <96593707+BlueSkyliner@users.noreply.github.com>
2020-06-09 16:01:02 +0000

    mtd: cafe_nand: fix an & vs | mistake

--- client/components/Carousel.tsx
+++ client/components/Carousel.tsx
@@ -1,1 +1,15 @@
-import { 
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Autoplay, Pagination, Navigation } from "swiper";
+import styles from "../styles/components/carousel.module.scss";
+import Link from "next/link";
+
+SwiperCore.use([Autoplay, Pagination, Navigation]);
+export default function Carousel() {
+  return (
+    <div className={styles["swiper-div"]}>
+      <Swiper
+        slidesPerView={2}
+        spaceBetween={22}
+        slidesPerGroup={1}
+        centeredSlides={true}
+   
