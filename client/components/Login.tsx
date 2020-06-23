7a312995124bc5c45f1f4c1ffc4265db18039f9d
BlueSkyliner <96593707+BlueSkyliner@users.noreply.github.com>
2020-06-23 19:30:32 +0000

    Fix argument

--- client/components/Login.tsx
+++ client/components/Login.tsx
@@ -4,4 +4,24 @@ import Setaxios from "../fetching/Setaxios";
 import Cookies from "js-cookie";
 import axios from "axios";
 
-export defau
+export default function Login({
+  handleLoginModal,
+  handleLoginStatus,
+  handleSignupModal,
+}: {
+  handleLoginModal(): void;
+  handleLoginStatus(): void;
+  handleSignupModal(): void;
+}) {
+  const [userData, setUserData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleInputValue =
+    (key: string) => (e: ChangeEvent<HTMLInputElement>) => {
+      setUserData({ ...userData, [key]: e.target.value });
+    };
+
+  async function LoginSubmit() {
+    const e
