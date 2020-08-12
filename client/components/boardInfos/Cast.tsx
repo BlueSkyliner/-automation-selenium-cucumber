84432f7e7be72fdc5b40ef8f36a68c106456f8bc
BlueSkyliner <96593707+BlueSkyliner@users.noreply.github.com>
2020-08-12 15:50:54 +0000

    Pontoon: Update Malay (ms) localization of AMO

--- client/components/boardInfos/Cast.tsx
+++ client/components/boardInfos/Cast.tsx
@@ -10,4 +10,24 @@ export default function Cast({ cast, onFocus }: any) {
       case 2:
         return <div>주연</div>;
       case 3:
-        re
+        return <div>조연</div>;
+    }
+  }
+  function Emptyimg(num: Number) {
+    switch (num) {
+      case 1:
+        return <img src="/Director.jpg" />;
+      case 2:
+        return <img src="/Lead.jpg" />;
+      case 3:
+        return <img src="/supporting.jpg" />;
+    }
+  }
+
+  return (
+    <div className={styles["cast"]}>
+      <div className={styles.head}>
+        <div>출연진</div>
+        {onFocus ? null : (
+          <button
+            className={style
