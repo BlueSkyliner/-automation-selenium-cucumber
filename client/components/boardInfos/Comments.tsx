d0c9b3ecad6714e96a41b3e7d4e7208d4e0674c0
BlueSkyliner <96593707+BlueSkyliner@users.noreply.github.com>
2020-09-08 07:06:14 +0000

    fix refs

--- client/components/boardInfos/Comments.tsx
+++ client/components/boardInfos/Comments.tsx
@@ -1,4 +1,17 @@
 import Setaxios from "../../fetching/Setaxios";
 import styles from "../../styles/components/boardInfos/comments.module.scss";
 
-export default fun
+export default function Comments({ comments }: { comments: Array<string> }) {
+  if (comments === null || comments.length === 0) {
+    return (
+      <div className={styles.head}>
+        코멘트
+        <div className={styles.nocomments}>
+          <div className={styles.message}>등록된 코멘트가 없습니다</div>
+        </div>
+      </div>
+    );
+  }
+  function sendLike(commentid: number) {
+    Setaxios.postAxios("rating", { commentId: Number(commentid) })
+ 
