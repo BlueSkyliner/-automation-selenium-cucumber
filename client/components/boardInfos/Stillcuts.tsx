5845f1c896603f7e3317c1735b2d32a96b9155ec
BlueSkyliner <96593707+BlueSkyliner@users.noreply.github.com>
2020-10-19 06:40:03 +0000

    Ignore .DS_Store files

--- client/components/boardInfos/Stillcuts.tsx
+++ client/components/boardInfos/Stillcuts.tsx
@@ -1,1 +1,13 @@
-import { useState } fro
+import { useState } from "react";
+import styles from "../../styles/components/boardInfos/stills.module.scss";
+
+export default function Stillcuts({ stills, onFocus }: any) {
+  let counter = 0;
+  const [pirctureModalOpen, setPictureModalOpen] = useState<boolean>(false);
+  const [selectedImage, setSelectedImage] = useState<any>();
+  const pictureModal = () => {
+    return (
+      <div key="picmodal">
+        <div
+          className={styles["screen-wrapper"]}
+          onClick={(e) => 
