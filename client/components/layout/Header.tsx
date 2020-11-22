3e24336a60378c60d19e956b0f432375f576261c
BlueSkyliner <96593707+BlueSkyliner@users.noreply.github.com>
2020-11-22 20:29:01 +0000

    so

--- client/components/layout/Header.tsx
+++ client/components/layout/Header.tsx
@@ -7,3 +7,11 @@ import Setaxios from "../../fetching/Setaxios";
 import { useRouter } from "next/router";
 import axios from "axios";
 import Cookies from "js-cookie";
+export default function Header() {
+  const [loginModalOpen, setLoginModalOpen] = useState<boolean>(false);
+  const [signupModalOpen, setSignupModalOpen] = useState<boolean>(false);
+  const [userLoginStatus, setUserLoginStatus] = useState<boolean>(false);
+  const [userAdminStatus, setUserAdminStatus] = useState<boolean>(false);
+  const [searchedData, setSearchedData] = useState([]);
+  const [searchedOnfocus, setSearchedOnfocus] = useState<boolean>(false);
+  const ro
