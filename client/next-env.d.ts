12625449f3bd372841c16e3d77396c777c3cf96d
BlueSkyliner <96593707+BlueSkyliner@users.noreply.github.com>
2021-01-25 17:01:50 +0000

    Fix icon path

--- client/next-env.d.ts
+++ client/next-env.d.ts
@@ -2,4 +2,5 @@
 /// <reference types="next/types/global" />
 /// <reference types="next/image-types/global" />
 
-// NOTE: Thi
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/basic-features/typescript for more information.
