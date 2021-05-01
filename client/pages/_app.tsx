0dcee20e41dfe9b313e6deda38c9a837ffa46b99
BlueSkyliner <96593707+BlueSkyliner@users.noreply.github.com>
2021-05-01 20:27:59 +0000

    Fix licence

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,2 +1,15 @@
 import "../styles/globals.css";
-import Layout from "../compo
+import Layout from "../components/layout/Layout";
+import { AppProps } from "next/dist/shared/lib/router/router";
+
+import "swiper/scss";
+import "swiper/scss/pagination";
+import "swiper/scss/navigation";
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
+}
---
+++ client/pages/approval.tsx
@@ -0,0 +1,4 @@
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import Setaxios from "../fetching/Setaxios";
+import st
