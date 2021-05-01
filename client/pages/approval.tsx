63a1a58d0ac5902f369e651fb79f43e67180d853
BlueSkyliner <96593707+BlueSkyliner@users.noreply.github.com>
2021-05-01 20:59:46 +0000

    Adding docs in reStructuredText

--- client/pages/approval.tsx
+++ client/pages/approval.tsx
@@ -1,4 +1,16 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import Setaxios from "../fetching/Setaxios";
-import st
+import styles from "../styles/approval.module.scss";
+import Cookies from "js-cookie";
+
+export default function Approval() {
+  const router = useRouter();
+  const [timer, setTimer] = useState(0);
+  useEffect(() => {
+    const timeout = setTimeout(() => setTimer(timer + 1), 1000);
+    if (!router.isReady) return () => clearTimeout(timeout);
+
+    Setaxios.postAxios("pay/approve", {
+      pg_token: router.query.pg_token,
+      boardId: Cookies.get("bo
