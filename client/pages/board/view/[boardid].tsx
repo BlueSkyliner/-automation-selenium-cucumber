c0e9665b2b4ad4c1d86ff1ca6b40c798926ca9e3
BlueSkyliner <96593707+BlueSkyliner@users.noreply.github.com>
2021-05-08 08:52:21 +0000

    modify README.

--- client/pages/board/view/[boardid].tsx
+++ client/pages/board/view/[boardid].tsx
@@ -5,4 +5,18 @@ import Rating from "../../../components/boardInfos/Rating";
 import Setaxios from "../../../fetching/Setaxios";
 import Cookies from "js-cookie";
 
-process.env.N
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+export default function BoarDetails({ film }: any) {
+  let filmData: any;
+  if (film) {
+    filmData = film.FindBoardId.data;
+  } else {
+    return <></>;
+  }
+  function Payment() {
+    Setaxios.getAxios(`pay/ready?amount=3000&board_id=${filmData.id}`)
+      .then((res) => {
+        Cookies.set("boardId", filmData.id);
+        const urlcomp: any = res.data;
+        Cookies.set("nexturl", urlcomp.data.next_redirect_pc_url
