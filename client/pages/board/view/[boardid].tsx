b005da2bd891f591613881d5114ae06f9339c235
BlueSkyliner <96593707+BlueSkyliner@users.noreply.github.com>
2021-05-09 09:55:13 +0000

    spi: spi-pxa2xx: Check status register to determine if SSSR_TINT is disabled

--- client/pages/board/view/[boardid].tsx
+++ client/pages/board/view/[boardid].tsx
@@ -19,4 +19,19 @@ export default function BoarDetails({ film }: any) {
       .then((res) => {
         Cookies.set("boardId", filmData.id);
         const urlcomp: any = res.data;
-        Cookies.set("nexturl", urlcomp.data.next_redirect_pc_url
+        Cookies.set("nexturl", urlcomp.data.next_redirect_pc_url);
+        const payment: Window | null = window.open(
+          urlcomp.data.next_redirect_pc_url,
+          "_blank",
+          "width=600,height=500"
+        );
+        if (payment === null) return;
+        payment.addEventListener("unload", () => {
+          location.reload();
+        });
+      })
+      .catch((err) => {
+        if (err.response.data.code === 4016) {
+          alert("먼저 영화에 대한 평을 작성해주세요!");
+        }
+       
