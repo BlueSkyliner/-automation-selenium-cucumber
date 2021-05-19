1087700a0933e9a87e7a33d659448b50f026d03c
BlueSkyliner <96593707+BlueSkyliner@users.noreply.github.com>
2021-05-19 14:46:58 +0000

    scripted change

--- client/pages/management.tsx
+++ client/pages/management.tsx
@@ -5,4 +5,13 @@ export default function Management() {
       <div className={styles.mapageblock}>
         <div className={styles.header__blank__div} />
         <div className={styles["myinfo-wrapper"]}>
-      
+          <div className={styles.myinfo}>
+            <div className={styles["info-header"]}>승인 대기중 보드 리스트</div>
+            <div className={styles.dividingline} />
+            <div className={styles["information"]}>
+              <div className={styles.blankarea} />
+              <div className={styles.blankedinfos}>
+                <div>
+                  <div className={styles.password}>
+                    <div className={styles.text}>
+                
