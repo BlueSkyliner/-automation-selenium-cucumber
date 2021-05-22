77b026dade90b46fb2d6bfa2bff71f6f8005649c
BlueSkyliner <96593707+BlueSkyliner@users.noreply.github.com>
2021-05-22 05:28:33 +0000

    updated package-lock.json

--- client/pages/mypage.tsx
+++ client/pages/mypage.tsx
@@ -88,3 +88,218 @@ export default function Mypage({ userInfo, film }: any) {
                 <div className={styles.border}>
                   <img src="/pay_logo.png" />
                 </div>
+              </div>
+              <div className={styles.donation}>
+                <div className={styles.amount}>₩{movie.donationAmount}</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    });
+  };
+  async function changeUserPwd() {
+    await Setaxios.putAxios("user", { password: changeValue })
+      .then((res) => {
+        alert("성공적으로 변경됐습니다");
+        location.reload();
+      })
+      .catch((err) => console.log(err.response));
+  }
+  async function changeUserNickname() {
+    await Setaxios.putAxios("user", { nickname: nickChangeValue })
+      .then((res) => {
+        alert("성공적으로 변경됐습니다");
+        location.reload();
+      })
+      .catch((err) => console.log(err.response));
+  }
+  return (
+    <div className={styles.mypage}>
+      <div className={styles.mapageblock}>
+        <div className={styles.header__blank__div} />
+        <div className={styles["myinfo-wrapper"]}>
+          <div className={styles.myinfo}>
+            <div className={styles.enroll}>
+              <div>
+                <Link href="/newboard">영화 등록하기</Link>
+              </div>
+            </div>
+            <div className={styles["info-header"]}>유저 정보</div>
+            {/* <button
+              onClick={() => {
+                Setaxios.deleteAxios("user")
+                  .then((res) => {
+                    alert("탈퇴처리가 이루어졌습니다");
+                  })
+                  .catch((err) => console.log(err));
+              }}
+            >
+              회원탈퇴
+            </button> */}
+            <div className={styles.dividingline} />
+            <div className={styles["information"]}>
+              <div className={styles.blankarea} />
+              <div className={styles.blankedinfos}>
+                <div className={styles.profilepic}>
+                  {userInfo.profile_picture ? (
+                    <img
+                      className={styles.profilePicture}
+                      src={userInfo.profile_picture}
+                    />
+                  ) : (
+                    <img src="/defaultprofile.png" />
+                  )}
+                  <label htmlFor="image-file-select">
+                    <img className={styles.inputfilebutton} src="/modify.png" />
+                  </label>
+                </div>
+                <div className={styles.fileselector}>
+                  <input
+                    id="image-file-select"
+                    type="file"
+                    onChange={(e) => {
+                      let fd = new FormData();
+                      let file: any = e.target;
+                      fd.append("upload", file.files[0]);
+                      Setaxios.postfileAxios(`image/user`, fd)
+                        .then((res) => location.reload())
+                        .catch((err) => console.log(err.response));
+                    }}
+                  />
+                </div>
+                <div>
+                  <div className={styles.email}>{userInfo.email}</div>
+                  <div className={styles.password}>
+                    <div className={styles.text}>
+                      <div>비밀번호 : **********</div>
+                    </div>
+                    <div className={styles.button}>
+                      <button
+                        onClick={() => setPasswordChange(!passwordChange)}
+                      >
+                        비밀번호 변경
+                      </button>
+                    </div>
+                  </div>
+                  {passwordChange ? (
+                    <div>
+                      <div>
+                        <div>현재 비밀번호</div>
+                        <input />
+                      </div>
+                      <div>
+                        <div>변경할 비밀번호</div>
+                        <input
+                          onChange={(e) => {
+                            let any: any = e.target;
+                            setChangeValue(any.value);
+                          }}
+                        />
+                      </div>
+                      <div>
+                        <div>변경할 비밀번호 재확인</div>
+                        <input />
+                      </div>
+                      <button onClick={changeUserPwd}>변경하기</button>
+                    </div>
+                  ) : null}
+                  <div className={styles.nickname}>
+                    <div className={styles.text}>
+                      <div>닉네임 : {userInfo.nickname}</div>
+                    </div>
+                    <div className={styles.button}>
+                      <button onClick={() => setNickChange(!nickChange)}>
+                        닉네임 변경
+                      </button>
+                    </div>
+                  </div>
+                  {nickChange ? (
+                    <div>
+                      {" "}
+                      <div>
+                        <div>변경할 닉네임</div>
+                        <input
+                          onChange={(e) => {
+                            let any: any = e.target;
+                            setnickChangeValue(any.value);
+                          }}
+                        />
+                      </div>
+                      <button onClick={changeUserNickname}>변경하기</button>{" "}
+                    </div>
+                  ) : null}
+                </div>
+              </div>
+            </div>
+            <div className={styles["info-header"]}>내가 후원한 영화</div>
+            <div className={styles.dividingline} />
+            {mydonations()}
+            <div className={styles["info-header"]}>내가 담아둔 영화</div>
+            <div className={styles.dividingline} />
+            {mybuckets()}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const cookies: any = context.req.headers.cookie;
+  if (cookies === undefined || cookies.length < 20) {
+    return { props: { userInfo: null } };
+  }
+  const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/user`, {
+    headers: {
+      "Content-Type": "application/json",
+      accesstoken: cookies?.slice(cookies?.search("accesstoken") + 12),
+    },
+  }).catch((err) => {
+    return err;
+  });
+
+  const query = `{
+    FindLikeBoards(Limit : 4) {
+      data {
+        id
+        title
+        posterImg
+        infoCountry
+        infoCreatedYear
+        infoTime
+        infoLimit
+        infoStory
+        genre{
+            name
+        }
+      }
+    }
+    FindDonationBoards(Limit : 4){
+        data{
+            id
+            title
+            donationAmount
+            donationCreatedAt
+        }
+    }
+}`;
+
+  const res2 = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/graphql`, {
+    method: "POST",
+    body: JSON.stringify({ query }),
+    headers: {
+      "Content-Type": "application/json",
+      accesstoken: cookies?.slice(cookies?.search("accesstoken") + 12),
+    },
+  }).catch((err) => {
+    return err;
+  });
+  let film = null;
+  let userData = null;
+  if (res2) film = await (await res2).json();
+  if (res) userData = await (await res).json();
+  return {
+    props: { userInfo: userData, film: film },
+  };
+};
