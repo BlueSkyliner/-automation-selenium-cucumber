4a931a4b7fba087b2e9eab0110ed2d034546a77e
BlueSkyliner <96593707+BlueSkyliner@users.noreply.github.com>
2021-07-05 22:20:10 +0000

    id sync

---
+++ server/src/main/java/com/IndieAn/GoFundIndie/Common/BoardPageOrderTypes.java
@@ -0,0 +1,13 @@
+package com.IndieAn.GoFundIndie.Common;
+
+public enum BoardPageOrderTypes {
+    // 인기순
+    BOARD_PAGE_ORDER_TYPES_POPULAR("Popular"),
+    // 가나다순 ( 사전순 )
+    BOARD_PAGE_ORDER_TYPES_DIC("Dictionary"),
+    // 최신순
+    BOARD_PAGE_ORDER_TYPES_NEW("New");
+
+    private final String types;
+
+  
