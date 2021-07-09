cce1ce5aff4727955055bb5e77c4a79fd26c4dba
BlueSkyliner <96593707+BlueSkyliner@users.noreply.github.com>
2021-07-09 19:40:31 +0000

    Commit from LuCI Translation Portal by user jow.: 47 of 47 messages translated (0 fuzzy).

--- server/src/main/java/com/IndieAn/GoFundIndie/Common/MethodArgumentNotValidExceptionHandler.java
+++ server/src/main/java/com/IndieAn/GoFundIndie/Common/MethodArgumentNotValidExceptionHandler.java
@@ -1,3 +1,10 @@
 package com.IndieAn.GoFundIndie.Common;
 
-import 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.support.MethodArgumentNotValidException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.
