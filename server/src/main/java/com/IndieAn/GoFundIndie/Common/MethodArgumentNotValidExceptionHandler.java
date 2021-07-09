b19242fd797dae0960b0e71843a7fec9671fbab1
BlueSkyliner <96593707+BlueSkyliner@users.noreply.github.com>
2021-07-09 23:02:58 +0000

    Edit pom

--- server/src/main/java/com/IndieAn/GoFundIndie/Common/MethodArgumentNotValidExceptionHandler.java
+++ server/src/main/java/com/IndieAn/GoFundIndie/Common/MethodArgumentNotValidExceptionHandler.java
@@ -7,4 +7,18 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+
+@Slf4j
+@ControllerAdvice
+@RestController
+public class MethodArgumentNotValidExceptionHandler {
+
+    // NotBlank Handler : 4006
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> processValidationError(MethodArgumentNotValidException exception) {
+        BindingResult bindingResult = exception.getBindingResult();
+        try {
+            fo
