2c04d0e5fd89edd26602850540b1541f46af6171
BlueSkyliner <96593707+BlueSkyliner@users.noreply.github.com>
2021-07-11 23:04:21 +0000

    Update custom.md

--- server/src/main/java/com/IndieAn/GoFundIndie/Config/PropertyConfig.java
+++ server/src/main/java/com/IndieAn/GoFundIndie/Config/PropertyConfig.java
@@ -1,3 +1,14 @@
 package com.IndieAn.GoFundIndie.Config;
 
-import org.springframework.beans.factory.config.PropertiesFacto
+import org.springframework.beans.factory.config.PropertiesFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+@Configuration
+public class PropertyConfig {
+
+    @Bean(name = "info")
+    public PropertiesFactoryBean propertiesFactoryBean() throws Exception {
+        PropertiesFactoryBean propertiesFactoryBean = new PropertiesFactoryBean();
+        ClassPathResource classPathResource = new ClassPath
