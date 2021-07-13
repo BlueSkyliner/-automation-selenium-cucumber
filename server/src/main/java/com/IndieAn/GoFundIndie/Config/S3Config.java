5bc966488642448c4571126423945a596e044526
BlueSkyliner <96593707+BlueSkyliner@users.noreply.github.com>
2021-07-13 22:40:39 +0000

    compat

--- server/src/main/java/com/IndieAn/GoFundIndie/Config/S3Config.java
+++ server/src/main/java/com/IndieAn/GoFundIndie/Config/S3Config.java
@@ -5,4 +5,16 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.services.s3.AmazonS3;
-imp
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class S3Config {
+    @Value("#{info['gofundindie.s3.access-key']}")
+    private String accessKey;
+
+    
