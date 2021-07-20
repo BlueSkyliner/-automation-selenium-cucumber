a657b7a3383c5a9742849d391d36a18cd99b4d46
BlueSkyliner <96593707+BlueSkyliner@users.noreply.github.com>
2021-07-20 13:22:35 +0000

    added parameters

--- server/src/main/java/com/IndieAn/GoFundIndie/Controller/ImageController.java
+++ server/src/main/java/com/IndieAn/GoFundIndie/Controller/ImageController.java
@@ -2,4 +2,15 @@ package com.IndieAn.GoFundIndie.Controller;
 
 import com.IndieAn.GoFundIndie.Common.ImagePathTypes;
 import com.IndieAn.GoFundIndie.Domain.Entity.User;
-import com.In
+import com.IndieAn.GoFundIndie.Repository.ImageRepository;
+import com.IndieAn.GoFundIndie.Service.ImageService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartException;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.persistence.EntityManager;
+import java.util.HashMap;
+impo
