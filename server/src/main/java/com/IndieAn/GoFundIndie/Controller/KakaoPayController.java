e1e8c7a4d7c31bc093aa1e065a40cdca1a8f2d53
BlueSkyliner <96593707+BlueSkyliner@users.noreply.github.com>
2021-08-01 18:46:52 +0000

    Builder/Provider/RooUsage.txt

--- server/src/main/java/com/IndieAn/GoFundIndie/Controller/KakaoPayController.java
+++ server/src/main/java/com/IndieAn/GoFundIndie/Controller/KakaoPayController.java
@@ -4,4 +4,20 @@ import com.IndieAn.GoFundIndie.Domain.DTO.KakaoPayApproveInputDTO;
 import com.IndieAn.GoFundIndie.Service.KakaoPayService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@Slf4j
+@RestController
+public class KakaoPayController {
+    private final KakaoPayService kakaoPayService;
+
+    @Autowired
+    public KakaoPayController(KakaoPayService kakaoPayService) {
+        this.kakaoPayService = kakaoPayService;
+    }
+
+    @GetMapping(value = "/pay/ready")
+    public ResponseEntity<?> Payment(@RequestParam("amount")
