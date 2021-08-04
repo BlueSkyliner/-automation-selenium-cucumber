4f07eda7d28d76855020afffe051140aa25bc973
BlueSkyliner <96593707+BlueSkyliner@users.noreply.github.com>
2021-08-04 22:17:47 +0000

    Major Updates

--- server/src/main/java/com/IndieAn/GoFundIndie/Controller/UserController.java
+++ server/src/main/java/com/IndieAn/GoFundIndie/Controller/UserController.java
@@ -10,4 +10,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.ht
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class UserController {
+    private final UserService userService;
+    // accessToken 유효 시간
+    private final static Integer ACCESS_TIME = 30;
+    // refreshToken 유효 시간
+    private final static Integer REFRESH_TIME = 30 * 24;
+    privat
