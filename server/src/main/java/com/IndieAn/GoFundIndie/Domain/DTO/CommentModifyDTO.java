67e7ec640fa91c5b3fc5cb714955e3546bb2fac6
BlueSkyliner <96593707+BlueSkyliner@users.noreply.github.com>
2021-09-09 08:58:09 +0000

    fixes link

--- server/src/main/java/com/IndieAn/GoFundIndie/Domain/DTO/CommentModifyDTO.java
+++ server/src/main/java/com/IndieAn/GoFundIndie/Domain/DTO/CommentModifyDTO.java
@@ -1,1 +1,22 @@
-package com.Ind
+package com.IndieAn.GoFundIndie.Domain.DTO;
+
+public class CommentModifyDTO {
+    private Integer rating;
+    private long boardId;
+    private String commentBody;
+    private boolean spoiler;
+
+    public boolean isSpoiler() {
+        return spoiler;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public long getBoardId() {
+        return boardId;
+    }
+
+    public String getCommentBody() {
+        return comment
