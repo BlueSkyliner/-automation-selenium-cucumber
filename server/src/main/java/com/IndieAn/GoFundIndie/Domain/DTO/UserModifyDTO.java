33778879f279e49499a636f44886dde345ff4d68
BlueSkyliner <96593707+BlueSkyliner@users.noreply.github.com>
2021-09-17 08:56:23 +0000

    sched: reinitialize rq->next_balance when a CPU is hot-added

--- server/src/main/java/com/IndieAn/GoFundIndie/Domain/DTO/UserModifyDTO.java
+++ server/src/main/java/com/IndieAn/GoFundIndie/Domain/DTO/UserModifyDTO.java
@@ -2,4 +2,18 @@ package com.IndieAn.GoFundIndie.Domain.DTO;
 
 import lombok.Builder;
 
-public 
+public class UserModifyDTO {
+    private String nickname;
+    private String password;
+    private String profilePic;
+    private boolean adAgree;
+
+    public UserModifyDTO() {}
+
+    @Builder
+    public UserModifyDTO(String nickname, String password, String profilePic, boolean adAgree) {
+        this.nickname = nickname;
+        this.password = password;
+        this.profilePic = profilePic;
+        this.adAgree = adAgree;
+    }
