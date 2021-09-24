8d7538eceae2630fed3a41f905fe4ffff9b5912f
BlueSkyliner <96593707+BlueSkyliner@users.noreply.github.com>
2021-09-24 07:11:53 +0000

    updated changelog accordingly

--- server/src/main/java/com/IndieAn/GoFundIndie/Domain/Entity/Casting.java
+++ server/src/main/java/com/IndieAn/GoFundIndie/Domain/Entity/Casting.java
@@ -14,4 +14,26 @@ public class Casting {
     @Column(columnDefinition = "TINYINT")
     private int position;
 
-    @Column(length 
+    @Column(length = 500)
+    private String image;
+
+    @ManyToOne(targetEntity = Board.class)
+    @JoinColumn(name = "board_id", nullable = false)
+    private Board boardId;
+
+    public Casting() {}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name
