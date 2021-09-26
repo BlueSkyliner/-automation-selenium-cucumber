d715c22c1b323aa35b7eb9ad3c6159af3463577d
BlueSkyliner <96593707+BlueSkyliner@users.noreply.github.com>
2021-09-26 08:58:55 +0000

    Fix compile

--- server/src/main/java/com/IndieAn/GoFundIndie/Domain/Entity/CommentRating.java
+++ server/src/main/java/com/IndieAn/GoFundIndie/Domain/Entity/CommentRating.java
@@ -9,4 +9,29 @@ public class CommentRating {
     private long id;
 
     @ManyToOne(targetEntity = User.class)
-    @JoinColumn(name = "user_id", nul
+    @JoinColumn(name = "user_id", nullable = false)
+    private User userId;
+
+    @ManyToOne(targetEntity = Comment.class)
+    @JoinColumn(name = "comment_id", nullable = false)
+    private Comment commentId;
+
+    public CommentRating() {}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public User getUserId() {
+        return userId;
+    }
+
+    public void setUserId(User userId) {
+        this.userId = userId;
+    }
+
+    public Comment getCommentId(
