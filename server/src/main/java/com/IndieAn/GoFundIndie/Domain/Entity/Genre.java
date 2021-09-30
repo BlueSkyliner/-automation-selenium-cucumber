17ba21a9c0a25751faaae76758c951e1ba5e7bad
BlueSkyliner <96593707+BlueSkyliner@users.noreply.github.com>
2021-09-30 15:09:02 +0000

    Adds comments

--- server/src/main/java/com/IndieAn/GoFundIndie/Domain/Entity/Genre.java
+++ server/src/main/java/com/IndieAn/GoFundIndie/Domain/Entity/Genre.java
@@ -1,3 +1,21 @@
 package com.IndieAn.GoFundIndie.Domain.Entity;
 
-import ja
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Genre {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "genreId", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private List<BoardGenre> boardGenres = new ArrayList<>();
+
+    public Genre() {}
+
+    public List<BoardGenre>
