ce8ea76b720a802782b92cbc903733935fec4421
BlueSkyliner <96593707+BlueSkyliner@users.noreply.github.com>
2021-10-02 11:27:46 +0000

    Whitespace cleanup.

--- server/src/main/java/com/IndieAn/GoFundIndie/Domain/Entity/Still.java
+++ server/src/main/java/com/IndieAn/GoFundIndie/Domain/Entity/Still.java
@@ -3,4 +3,23 @@ package com.IndieAn.GoFundIndie.Domain.Entity;
 import javax.persistence.*;
 
 @Entity
-public cl
+public class Still {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, length = 500)
+    private String image;
+
+    @ManyToOne(targetEntity = Board.class)
+    @JoinColumn(name = "board_id", nullable = false)
+    private Board boardId;
+
+    public Still() {}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+    
