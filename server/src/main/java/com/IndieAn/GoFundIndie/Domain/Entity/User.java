d2a6b6899391f0a0baef7b76b05064af93ebc278
BlueSkyliner <96593707+BlueSkyliner@users.noreply.github.com>
2021-10-03 09:07:05 +0000

    Publish page-4 index

--- server/src/main/java/com/IndieAn/GoFundIndie/Domain/Entity/User.java
+++ server/src/main/java/com/IndieAn/GoFundIndie/Domain/Entity/User.java
@@ -1,4 +1,21 @@
 package com.IndieAn.GoFundIndie.Domain.Entity;
 
 import com.IndieAn.GoFundIndie.Resolvers.DTO.User.UserGraphQLDTO;
-import jav
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "admin_role", columnDefinition = "boolean default false")
+    private boolean adminRole;
+
+    @Column(columnDefinition = "boolean default false")
+    private boolean banned;
+
+    @Column(nullable = false
