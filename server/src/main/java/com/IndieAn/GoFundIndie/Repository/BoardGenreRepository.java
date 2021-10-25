ba2e35444cdb64505ca8a47564c35ed0e219ae46
BlueSkyliner <96593707+BlueSkyliner@users.noreply.github.com>
2021-10-25 11:46:57 +0000

    msm: camera: isp: Validate input parameter for vfe_write and vfe_read

--- server/src/main/java/com/IndieAn/GoFundIndie/Repository/BoardGenreRepository.java
+++ server/src/main/java/com/IndieAn/GoFundIndie/Repository/BoardGenreRepository.java
@@ -2,4 +2,19 @@ package com.IndieAn.GoFundIndie.Repository;
 
 import com.IndieAn.GoFundIndie.Domain.Entity.Board;
 import com.IndieAn.GoFundIndie.Domain.Entity.BoardGenre;
-import com.
+import com.IndieAn.GoFundIndie.Domain.Entity.Genre;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+@Slf4j
+@Repository
+@Transactional
+@RequiredArgsConstructor
+public class BoardGenreRepository extends EntityManagerExtend{
+    private final EntityManager entityManager;
+
+    priva
