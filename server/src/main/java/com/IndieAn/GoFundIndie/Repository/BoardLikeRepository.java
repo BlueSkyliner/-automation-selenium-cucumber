77ba75152142f92ec4b4d85852eb2e2fa5cde3d6
BlueSkyliner <96593707+BlueSkyliner@users.noreply.github.com>
2021-10-27 07:37:27 +0000

    Major bug fixes

--- server/src/main/java/com/IndieAn/GoFundIndie/Repository/BoardLikeRepository.java
+++ server/src/main/java/com/IndieAn/GoFundIndie/Repository/BoardLikeRepository.java
@@ -4,4 +4,20 @@ import com.IndieAn.GoFundIndie.Domain.Entity.Board;
 import com.IndieAn.GoFundIndie.Domain.Entity.BoardLike;
 import com.IndieAn.GoFundIndie.Domain.Entity.User;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.util.Date;
+import java.util.List;
+
+@Slf4j
+@Repository
+@Transactional
+@RequiredArgsConstructor
+public class BoardLikeRepository extends EntityManagerExtend{
+    private final EntityManager entityManager;
+
+    public boolean isLikedBoard(User user, Board board) {
+        return entityManager.creat
