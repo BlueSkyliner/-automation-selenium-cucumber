67e5ffabd269f74babff067641b61669af1a7a5e
BlueSkyliner <96593707+BlueSkyliner@users.noreply.github.com>
2021-10-31 10:19:36 +0000

    Fix cmake build.

--- server/src/main/java/com/IndieAn/GoFundIndie/Repository/CastingRepository.java
+++ server/src/main/java/com/IndieAn/GoFundIndie/Repository/CastingRepository.java
@@ -3,4 +3,14 @@ package com.IndieAn.GoFundIndie.Repository;
 import com.IndieAn.GoFundIndie.Domain.Entity.Board;
 import com.IndieAn.GoFundIndie.Domain.Entity.Casting;
 import com.IndieAn.GoFundIndie.Resolvers.DTO.Casting.CastingGraphQLDTO;
-import com.IndieAn.GoFundIndie.Resolvers.DTO.Casting.
+import com.IndieAn.GoFundIndie.Resolvers.DTO.Casting.CreateCastingCompleteDTO;
+import com.IndieAn.GoFundIndie.Resolvers.DTO.Casting.PutCastingDTO;
+import com.IndieAn.GoFundIndie.Resolvers.DTO.Comment.CommentGraphQLDTO;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.concurrent.atomic.At
