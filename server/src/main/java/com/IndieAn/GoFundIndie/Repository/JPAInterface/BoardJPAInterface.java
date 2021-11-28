e50d7b70fd0c83458c3c84f8a1b0a064b3482aed
BlueSkyliner <96593707+BlueSkyliner@users.noreply.github.com>
2021-11-28 06:41:24 +0000

    class billetera

--- server/src/main/java/com/IndieAn/GoFundIndie/Repository/JPAInterface/BoardJPAInterface.java
+++ server/src/main/java/com/IndieAn/GoFundIndie/Repository/JPAInterface/BoardJPAInterface.java
@@ -3,4 +3,5 @@ package com.IndieAn.GoFundIndie.Repository.JPAInterface;
 import com.IndieAn.GoFundIndie.Domain.Entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BoardJPAInterfac
+public interface BoardJPAInterface extends JpaRepository<Board, Long> {
+}
