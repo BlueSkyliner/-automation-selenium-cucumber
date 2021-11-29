0f048ecf2ff12194c84aaee3155bb63ccc56d2ba
BlueSkyliner <96593707+BlueSkyliner@users.noreply.github.com>
2021-11-29 10:13:11 +0000

    Automatic push

--- server/src/main/java/com/IndieAn/GoFundIndie/Resolvers/DTO/Board/BoardPagingDTO.java
+++ server/src/main/java/com/IndieAn/GoFundIndie/Resolvers/DTO/Board/BoardPagingDTO.java
@@ -8,4 +8,14 @@ import java.util.List;
 public class BoardPagingDTO {
     private Integer countBoards;
     private Integer countPages;
-    private Integer boardsP
+    private Integer boardsPerPage;
+    private Integer currentPage;
+    private List<BoardGraphQLDTO> content;
+
+    public BoardPagingDTO() {}
+
+    @Builder
+    public BoardPagingDTO(Integer countBoards, Integer countPages, Integer boardsPerPage, Integer currentPage, List<BoardGraphQLDTO> content) {
+        Assert.notNull(countBoards, "countBoards not null");
+        Assert.notNull(countPages, "countPages not null");
+        Assert.notNull(b
