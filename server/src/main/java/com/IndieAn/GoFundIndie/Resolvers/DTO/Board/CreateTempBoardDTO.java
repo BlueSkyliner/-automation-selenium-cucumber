cba5ee4c7bbe00104333dceee5c3d889c3dfc9bf
BlueSkyliner <96593707+BlueSkyliner@users.noreply.github.com>
2021-12-02 14:27:29 +0000

    [Mod]minor fix

--- server/src/main/java/com/IndieAn/GoFundIndie/Resolvers/DTO/Board/CreateTempBoardDTO.java
+++ server/src/main/java/com/IndieAn/GoFundIndie/Resolvers/DTO/Board/CreateTempBoardDTO.java
@@ -3,4 +3,18 @@ package com.IndieAn.GoFundIndie.Resolvers.DTO.Board;
 import lombok.Builder;
 
 @Builder
-publi
+public class CreateTempBoardDTO {
+    private long id;
+
+    public CreateTempBoardDTO() {}
+
+    public CreateTempBoardDTO(long id) { this.id = id; }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
