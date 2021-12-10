ffec85e5a90db050dcb3fca1ffa099057adaf5e7
BlueSkyliner <96593707+BlueSkyliner@users.noreply.github.com>
2021-12-10 18:16:22 +0000

    perf session: Do not fail on processing out of order event

--- server/src/main/java/com/IndieAn/GoFundIndie/Resolvers/DTO/Board/WrappingBoardGraphQLsDTO.java
+++ server/src/main/java/com/IndieAn/GoFundIndie/Resolvers/DTO/Board/WrappingBoardGraphQLsDTO.java
@@ -1,1 +1,21 @@
-package com.IndieAn.GoFundIndie.Resol
+package com.IndieAn.GoFundIndie.Resolvers.DTO.Board;
+
+import lombok.Builder;
+import org.springframework.util.Assert;
+
+import java.util.List;
+
+public class WrappingBoardGraphQLsDTO {
+    private int code;
+    private List<BoardGraphQLDTO> data;
+
+    public WrappingBoardGraphQLsDTO() {}
+
+    @Builder
+    public WrappingBoardGraphQLsDTO(int code, List<BoardGraphQLDTO> data) {
+        this.code = code;
+        this.data = data;
+    }
+
+    public int getCode() {
+ 
