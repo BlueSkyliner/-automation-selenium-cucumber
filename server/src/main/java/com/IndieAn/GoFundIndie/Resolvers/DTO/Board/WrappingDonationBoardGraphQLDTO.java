0b3ebf8798e1500a5e778e0c2cc26c4f08308bd1
BlueSkyliner <96593707+BlueSkyliner@users.noreply.github.com>
2021-12-16 17:20:33 +0000

    :gem: Travis CI image/link in readme :gem:

--- server/src/main/java/com/IndieAn/GoFundIndie/Resolvers/DTO/Board/WrappingDonationBoardGraphQLDTO.java
+++ server/src/main/java/com/IndieAn/GoFundIndie/Resolvers/DTO/Board/WrappingDonationBoardGraphQLDTO.java
@@ -10,3 +10,25 @@ public class WrappingDonationBoardGraphQLDTO {
     private List<DonationBoardGraphQLDTO> data; // nullable
 
     public WrappingDonationBoardGraphQLDTO() {}
+
+    @Builder
+    public WrappingDonationBoardGraphQLDTO(Integer code, List<DonationBoardGraphQLDTO> data) {
+        Assert.notNull(code, "code is not null");
+
+        this.code = code;
+        this.data = data;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public List<DonationBoardGraphQLDTO> getData() {
+        return data;
+    }
+
+    public void setData(List<Donat
