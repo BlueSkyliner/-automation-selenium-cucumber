a09741f312fb3221c1b3643055e91bb664d195e3
BlueSkyliner <96593707+BlueSkyliner@users.noreply.github.com>
2021-12-21 13:03:02 +0000

    Update es.po

--- server/src/main/java/com/IndieAn/GoFundIndie/Resolvers/DTO/Board/WrappingRandomBoardsDTO.java
+++ server/src/main/java/com/IndieAn/GoFundIndie/Resolvers/DTO/Board/WrappingRandomBoardsDTO.java
@@ -5,4 +5,27 @@ import org.springframework.util.Assert;
 
 import java.util.List;
 
-public class Wrapping
+public class WrappingRandomBoardsDTO {
+    private Integer code;
+    private List<RandomBoardDTO> data;
+
+    public WrappingRandomBoardsDTO() {
+    }
+
+    @Builder
+    public WrappingRandomBoardsDTO(Integer code, List<RandomBoardDTO> data) {
+        Assert.notNull(code, "code is not null");
+
+        this.code = code;
+        this.data = data;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public List<RandomBoar
