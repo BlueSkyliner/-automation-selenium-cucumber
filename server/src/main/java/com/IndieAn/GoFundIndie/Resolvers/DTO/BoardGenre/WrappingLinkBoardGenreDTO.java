5be1a0644efef221ffaee997ab666e85c08ccdaa
BlueSkyliner <96593707+BlueSkyliner@users.noreply.github.com>
2021-12-25 15:00:00 +0000

    Add babel-polyfill

--- server/src/main/java/com/IndieAn/GoFundIndie/Resolvers/DTO/BoardGenre/WrappingLinkBoardGenreDTO.java
+++ server/src/main/java/com/IndieAn/GoFundIndie/Resolvers/DTO/BoardGenre/WrappingLinkBoardGenreDTO.java
@@ -7,4 +7,26 @@ public class WrappingLinkBoardGenreDTO {
     private int code;
     private LinkBoardGenreDTO data;
 
-    public Wrap
+    public WrappingLinkBoardGenreDTO() {}
+
+    public WrappingLinkBoardGenreDTO(int code, LinkBoardGenreDTO data) {
+        this.code = code;
+        this.data = data;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public LinkBoardGenreDTO getData() {
+        return data;
+    }
+
+    public void setData(LinkBoardGenreDTO data) {
+        this.data = data;
+    }
+}
---
+++ server/src/main/java/com/IndieAn/GoFundIndie/Resolvers/DTO/BoardReport/BoardReportGraphQLDTO.java
@@ -0,0 +1,11 @@
+
+package com.IndieAn.GoFundIndie.Resolvers.DTO.BoardReport;
+
+import com.IndieAn.GoFundIndie.Domain.Entity.Board;
+import com.IndieAn.GoFundIndie.Domain.Entity.BoardReport;
+import com.IndieAn.GoFundIndie.Domain.Entity.User;
+import lombok.Builder;
+import org.springframework.util.Assert;
+
+public class BoardReportGraphQLDTO {
+    private Long id;
