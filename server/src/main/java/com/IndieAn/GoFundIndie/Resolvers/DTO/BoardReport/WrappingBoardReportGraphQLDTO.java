45e2c237e7ba69f40aa17d384aa9785064edafa8
BlueSkyliner <96593707+BlueSkyliner@users.noreply.github.com>
2021-12-27 06:49:57 +0000

    cfq-iosched: fix the setting of IOPS mode on SSDs

--- server/src/main/java/com/IndieAn/GoFundIndie/Resolvers/DTO/BoardReport/WrappingBoardReportGraphQLDTO.java
+++ server/src/main/java/com/IndieAn/GoFundIndie/Resolvers/DTO/BoardReport/WrappingBoardReportGraphQLDTO.java
@@ -7,4 +7,26 @@ public class WrappingBoardReportGraphQLDTO {
     private BoardReportGraphQLDTO data;
 
     @Builder
-    public WrappingBoardReportGraphQLDTO(int code, BoardReportGraphQLDT
+    public WrappingBoardReportGraphQLDTO(int code, BoardReportGraphQLDTO data) {
+        this.code = code;
+        this.data = data;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public BoardReportGraphQLDTO getData() {
+        return data;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public void setData(BoardReportGraphQLDTO data) {
+        this.data = data;
+    }
+
+    public static WrappingBoardReportGraphQLDTO bad(int code) {
+        return WrappingBoardReportGraphQLDTO.builde
