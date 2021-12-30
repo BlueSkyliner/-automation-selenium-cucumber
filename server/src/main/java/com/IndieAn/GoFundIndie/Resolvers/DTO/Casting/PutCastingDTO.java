ee222522d4e2be5eb6363230a4e6b50cc46f0afb
BlueSkyliner <96593707+BlueSkyliner@users.noreply.github.com>
2021-12-30 17:45:44 +0000

    - updated documentation

--- server/src/main/java/com/IndieAn/GoFundIndie/Resolvers/DTO/Casting/PutCastingDTO.java
+++ server/src/main/java/com/IndieAn/GoFundIndie/Resolvers/DTO/Casting/PutCastingDTO.java
@@ -6,4 +6,24 @@ import lombok.Builder;
 public class PutCastingDTO {
     private long castingId;
     private String name;
-    private int posit
+    private int position;
+    private String image;
+
+    public PutCastingDTO() {}
+
+    public PutCastingDTO(long castingId, String name, int position, String image) {
+        this.castingId = castingId;
+        this.name = name;
+        this.position = position;
+        this.image = image;
+    }
+
+    public long getCastingId() {
+        return castingId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosi
