a4deb7f75a6a47af9b3ba15f658cefbdc3c42f50
BlueSkyliner <96593707+BlueSkyliner@users.noreply.github.com>
2021-12-31 19:38:19 +0000

    ......increment version number

--- server/src/main/java/com/IndieAn/GoFundIndie/Resolvers/DTO/Casting/WrappingCreateTempCastingDTO.java
+++ server/src/main/java/com/IndieAn/GoFundIndie/Resolvers/DTO/Casting/WrappingCreateTempCastingDTO.java
@@ -1,1 +1,15 @@
-package com.IndieAn.GoFundIndie.Resol
+package com.IndieAn.GoFundIndie.Resolvers.DTO.Casting;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class WrappingCreateTempCastingDTO {
+    private int code;
+    private CreateTempCastingDTO data;
+
+    public WrappingCreateTempCastingDTO() {}
+}
---
+++ server/src/main/java/com/IndieAn/GoFundIndie/Resolvers/DTO/Genre/GenreGraphQLDTO.java
@@ -0,0 +1,1 @@
+package com.IndieAn.GoFundIn
