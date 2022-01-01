b1a81ca9bd03383828d04fa807ebacb03781aa17
BlueSkyliner <96593707+BlueSkyliner@users.noreply.github.com>
2022-01-01 14:49:39 +0000

    Release 1.2.3

--- server/src/main/java/com/IndieAn/GoFundIndie/Resolvers/DTO/Genre/GenreGraphQLDTO.java
+++ server/src/main/java/com/IndieAn/GoFundIndie/Resolvers/DTO/Genre/GenreGraphQLDTO.java
@@ -1,1 +1,24 @@
-package com.IndieAn.GoFundIn
+package com.IndieAn.GoFundIndie.Resolvers.DTO.Genre;
+
+import com.IndieAn.GoFundIndie.Domain.Entity.Genre;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
+@Builder
+@AllArgsConstructor
+public class GenreGraphQLDTO {
+    private long id;
+    private String name;
+
+    public GenreGraphQLDTO() {}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+     
