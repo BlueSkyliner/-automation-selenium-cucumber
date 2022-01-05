5f64e8978a54839e4c8696cefb705ca58f0d9ccf
BlueSkyliner <96593707+BlueSkyliner@users.noreply.github.com>
2022-01-05 09:26:48 +0000

    Minor UI fix

--- server/src/main/java/com/IndieAn/GoFundIndie/Resolvers/DTO/GqlResponseCodeIdDTO.java
+++ server/src/main/java/com/IndieAn/GoFundIndie/Resolvers/DTO/GqlResponseCodeIdDTO.java
@@ -6,3 +6,25 @@ public class GqlResponseCodeIdDTO {
     private int code;
     private long id;
 
+    @Builder
+    public GqlResponseCodeIdDTO(int code, long id) {
+        this.code = code;
+        this.id = id;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
