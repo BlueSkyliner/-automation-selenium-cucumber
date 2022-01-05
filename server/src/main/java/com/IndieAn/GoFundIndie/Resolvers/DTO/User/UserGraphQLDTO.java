f7abe857b43a87d0fc65310d70f38fe92d21099b
BlueSkyliner <96593707+BlueSkyliner@users.noreply.github.com>
2022-01-05 13:53:06 +0000

    Fix namespacing

--- server/src/main/java/com/IndieAn/GoFundIndie/Resolvers/DTO/User/UserGraphQLDTO.java
+++ server/src/main/java/com/IndieAn/GoFundIndie/Resolvers/DTO/User/UserGraphQLDTO.java
@@ -1,1 +1,17 @@
-package com.IndieAn.GoFundIndie.Resolver
+package com.IndieAn.GoFundIndie.Resolvers.DTO.User;
+
+import com.IndieAn.GoFundIndie.Domain.Entity.User;
+import lombok.*;
+
+@Getter
+@Setter
+public class UserGraphQLDTO {
+    private long id;
+    private boolean adminRole; //default exist
+    private boolean banned; //default exist
+    private String email; //non-null
+    private String password; //non-null
+    private String profilePicture;
+    private String nickname; //non-null
+    private String createdAt; // default exist
+   
