52cfc36bfb8b51a4206b5de63b16e1217a430eea
BlueSkyliner <96593707+BlueSkyliner@users.noreply.github.com>
2022-01-09 12:15:07 +0000

    add travis build status badge

--- server/src/main/java/com/IndieAn/GoFundIndie/Resolvers/Mutation.java
+++ server/src/main/java/com/IndieAn/GoFundIndie/Resolvers/Mutation.java
@@ -2,4 +2,9 @@ package com.IndieAn.GoFundIndie.Resolvers;
 
 import com.IndieAn.GoFundIndie.Resolvers.DTO.Board.CreateBoardCompleteDTO;
 import com.IndieAn.GoFundIndie.Resolvers.DTO.Board.PutBoardDTO;
-import com.IndieAn.GoFundIndie.Resolv
+import com.IndieAn.GoFundIndie.Resolvers.DTO.Board.WrappingCreateTempBoardDTO;
+import com.IndieAn.GoFundIndie.Resolvers.DTO.BoardGenre.WrappingLinkBoardGenreDTO;
+import com.IndieAn.GoFundIndie.Resolvers.DTO.BoardReport.CreateBoardReportDTO;
+import com.IndieAn.GoFundIndie.Resolvers.DTO.Casting.CreateCastingCompleteDTO;
+import com.IndieAn.GoFundIndie.Resolvers.DTO.Casting.PutCastingDTO;
+import com.IndieAn.GoFundIndie.Resolvers.DTO.Casting.Wrap
