b27e1b485b9be18d5a06cfcfc59ec8eb9916e5c5
BlueSkyliner <96593707+BlueSkyliner@users.noreply.github.com>
2022-01-10 12:41:16 +0000

    ignore temp files

--- server/src/main/java/com/IndieAn/GoFundIndie/Resolvers/Mutation.java
+++ server/src/main/java/com/IndieAn/GoFundIndie/Resolvers/Mutation.java
@@ -7,4 +7,12 @@ import com.IndieAn.GoFundIndie.Resolvers.DTO.BoardGenre.WrappingLinkBoardGenreDT
 import com.IndieAn.GoFundIndie.Resolvers.DTO.BoardReport.CreateBoardReportDTO;
 import com.IndieAn.GoFundIndie.Resolvers.DTO.Casting.CreateCastingCompleteDTO;
 import com.IndieAn.GoFundIndie.Resolvers.DTO.Casting.PutCastingDTO;
-import com.IndieAn.GoFundIndie.Resolvers.DTO.Casting.Wrap
+import com.IndieAn.GoFundIndie.Resolvers.DTO.Casting.WrappingCreateTempCastingDTO;
+import com.IndieAn.GoFundIndie.Resolvers.DTO.Genre.GenreGraphQLDTO;
+import com.IndieAn.GoFundIndie.Resolvers.DTO.GqlResponseCodeDTO;
+import com.IndieAn.GoFundIndie.Resolvers.DTO.User.UserGraphQLDTO;
+import com.IndieAn.GoFundIndie.Resolvers.Mutations.*;
+import graphql.kickstart.tools.GraphQLMutationResolver;
+import graphql.schema.DataFetchingEnvironment;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Sl
