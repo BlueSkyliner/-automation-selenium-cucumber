41fcac67773e7c3a328b60d4b37e7d6a83bc3ea3
BlueSkyliner <96593707+BlueSkyliner@users.noreply.github.com>
2022-01-20 15:59:08 +0000

    ch15

--- server/src/main/java/com/IndieAn/GoFundIndie/Resolvers/Mutations/BoardGenreMutation.java
+++ server/src/main/java/com/IndieAn/GoFundIndie/Resolvers/Mutations/BoardGenreMutation.java
@@ -4,4 +4,11 @@ import com.IndieAn.GoFundIndie.Domain.Entity.Board;
 import com.IndieAn.GoFundIndie.Domain.Entity.Genre;
 import com.IndieAn.GoFundIndie.Domain.Entity.User;
 import com.IndieAn.GoFundIndie.Repository.BoardGenreRepository;
-import 
+import com.IndieAn.GoFundIndie.Resolvers.DTO.BoardGenre.LinkBoardGenreDTO;
+import com.IndieAn.GoFundIndie.Resolvers.DTO.BoardGenre.WrappingLinkBoardGenreDTO;
+import com.IndieAn.GoFundIndie.Service.BoardService;
+import com.IndieAn.GoFundIndie.Service.GenreService;
+import com.IndieAn.GoFundIndie.Service.GqlUserValidService;
+import com.IndieAn.GoFundIndie.Service.UserService;
+import graphql.kickstart.servlet.context.GraphQLServletContext;
+import graphql.
