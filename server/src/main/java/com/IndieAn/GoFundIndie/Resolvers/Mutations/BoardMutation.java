dc4659852c04460c721b7288a849c1e611288eb2
BlueSkyliner <96593707+BlueSkyliner@users.noreply.github.com>
2022-01-27 18:45:04 +0000

    Add nav

--- server/src/main/java/com/IndieAn/GoFundIndie/Resolvers/Mutations/BoardMutation.java
+++ server/src/main/java/com/IndieAn/GoFundIndie/Resolvers/Mutations/BoardMutation.java
@@ -7,4 +7,17 @@ import com.IndieAn.GoFundIndie.Repository.BoardRepository;
 import com.IndieAn.GoFundIndie.Repository.UserRepository;
 import com.IndieAn.GoFundIndie.Resolvers.DTO.Board.*;
 import com.IndieAn.GoFundIndie.Resolvers.DTO.GqlResponseCodeDTO;
-import com.Indie
+import com.IndieAn.GoFundIndie.Service.BoardService;
+import com.IndieAn.GoFundIndie.Service.GqlUserValidService;
+import com.IndieAn.GoFundIndie.Service.UserService;
+import graphql.schema.DataFetchingEnvironment;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class BoardMut
