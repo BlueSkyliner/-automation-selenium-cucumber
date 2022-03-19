66f32803fb9a5c7d5e368ae4c61ed71756879ca2
BlueSkyliner <96593707+BlueSkyliner@users.noreply.github.com>
2022-03-19 22:48:50 +0000

    animation

---
+++ server/src/main/java/com/IndieAn/GoFundIndie/Resolvers/Querys/BoardQuery.java
@@ -0,0 +1,5 @@
+package com.IndieAn.GoFundIndie.Resolvers.Querys;
+
+import com.IndieAn.GoFundIndie.Common.SearchTypes;
+import com.IndieAn.GoFundIndie.Domain.Entity.Board;
+import com.IndieAn.GoFundIndie.Domain.Entity.
