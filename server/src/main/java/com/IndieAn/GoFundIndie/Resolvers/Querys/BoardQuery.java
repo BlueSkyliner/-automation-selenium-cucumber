5975d24f833c70603e29b90fb66871ffeec723fb
BlueSkyliner <96593707+BlueSkyliner@users.noreply.github.com>
2022-03-22 14:28:58 +0000

    update translation stats

--- server/src/main/java/com/IndieAn/GoFundIndie/Resolvers/Querys/BoardQuery.java
+++ server/src/main/java/com/IndieAn/GoFundIndie/Resolvers/Querys/BoardQuery.java
@@ -11,4 +11,19 @@ import com.IndieAn.GoFundIndie.Service.UserService;
 import graphql.kickstart.servlet.context.GraphQLServletContext;
 import graphql.schema.DataFetchingEnvironment;
 import lombok.RequiredArgsConstructor;
-i
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class BoardQuery {
+    private final BoardRepository boardRe
