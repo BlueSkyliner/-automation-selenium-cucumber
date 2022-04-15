d5fe79cb669662554008847d5c09d02e569d26c9
BlueSkyliner <96593707+BlueSkyliner@users.noreply.github.com>
2022-04-15 20:02:31 +0000

    modified license

--- server/src/main/java/com/IndieAn/GoFundIndie/Resolvers/Querys/BoardReportQuery.java
+++ server/src/main/java/com/IndieAn/GoFundIndie/Resolvers/Querys/BoardReportQuery.java
@@ -3,3 +3,15 @@ package com.IndieAn.GoFundIndie.Resolvers.Querys;
 import com.IndieAn.GoFundIndie.Domain.Entity.User;
 import com.IndieAn.GoFundIndie.Repository.BoardReportRepository;
 import com.IndieAn.GoFundIndie.Resolvers.DTO.BoardReport.WrappingBoardReportGraphQLDTO;
+import com.IndieAn.GoFundIndie.Resolvers.DTO.BoardReport.WrappingBoardReportsGraphqlDTO;
+import com.IndieAn.GoFundIndie.Service.GqlUserValidService;
+import graphql.schema.DataFetchingEnvironment;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@Transactional
+@Requi
