fcd100792276eca89a0fbfbd498eb7a397435998
BlueSkyliner <96593707+BlueSkyliner@users.noreply.github.com>
2022-04-21 14:07:05 +0000

    Update swlinks.txt

--- server/src/main/java/com/IndieAn/GoFundIndie/Resolvers/Querys/BoardSearchQuery.java
+++ server/src/main/java/com/IndieAn/GoFundIndie/Resolvers/Querys/BoardSearchQuery.java
@@ -2,4 +2,19 @@ package com.IndieAn.GoFundIndie.Resolvers.Querys;
 
 import com.IndieAn.GoFundIndie.Repository.BoardSearchRepository;
 import com.IndieAn.GoFundIndie.Resolvers.DTO.Board.SearchBoardDTO;
-import com.IndieAn.GoFundIndie.Resolvers.DTO.Board.WrappingSear
+import com.IndieAn.GoFundIndie.Resolvers.DTO.Board.WrappingSearchBoardDTO;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class BoardSearchQuery {
+    private final BoardSearchRepository boardSearchRepository;
+
+    private final char[] consonant = "ㄱㄲㄴㄷㄸㄹㅁㅂㅃㅅㅆㅇㅈㅉㅊㅋㅌㅍㅎ".toCharArray();
+    private final char[] middWords = "가까나다따라마바빠사싸아자짜차카타파하".t
