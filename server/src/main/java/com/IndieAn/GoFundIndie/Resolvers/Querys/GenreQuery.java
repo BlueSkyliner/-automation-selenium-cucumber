3463c1f1e47a1686e41772b1af24c93b28f65d17
BlueSkyliner <96593707+BlueSkyliner@users.noreply.github.com>
2022-04-24 21:18:29 +0000

    Draft update

--- server/src/main/java/com/IndieAn/GoFundIndie/Resolvers/Querys/GenreQuery.java
+++ server/src/main/java/com/IndieAn/GoFundIndie/Resolvers/Querys/GenreQuery.java
@@ -4,4 +4,18 @@ import com.IndieAn.GoFundIndie.Repository.GenreRepository;
 import com.IndieAn.GoFundIndie.Resolvers.DTO.Genre.GenreGraphQLDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframe
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class GenreQuery {
+    private final GenreRepository genreRepository;
+
+    public GenreGraphQLDTO FindGenreId(Long id) {
+        return GenreGraphQLDTO.from(genreRepository.FindByI
