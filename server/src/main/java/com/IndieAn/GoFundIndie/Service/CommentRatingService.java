56c4294f68376a9b4c525c8e56fbeb3d61e80d42
BlueSkyliner <96593707+BlueSkyliner@users.noreply.github.com>
2022-04-27 13:51:49 +0000

    new seh

--- server/src/main/java/com/IndieAn/GoFundIndie/Service/CommentRatingService.java
+++ server/src/main/java/com/IndieAn/GoFundIndie/Service/CommentRatingService.java
@@ -1,3 +1,14 @@
 package com.IndieAn.GoFundIndie.Service;
 
-import com.IndieAn.GoFundInd
+import com.IndieAn.GoFundIndie.Domain.Entity.CommentRating;
+import com.IndieAn.GoFundIndie.Domain.Entity.User;
+import com.IndieAn.GoFundIndie.Repository.CommentRatingRepository;
+import com.IndieAn.GoFundIndie.Repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+
+@Service
+public class CommentRatingService {
+    private final CommentRatin
