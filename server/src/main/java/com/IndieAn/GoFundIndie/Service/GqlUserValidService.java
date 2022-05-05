b005d11ec68b1165855d3c0ddff217b204f327f5
BlueSkyliner <96593707+BlueSkyliner@users.noreply.github.com>
2022-05-05 15:32:48 +0000

    update Procfile

--- server/src/main/java/com/IndieAn/GoFundIndie/Service/GqlUserValidService.java
+++ server/src/main/java/com/IndieAn/GoFundIndie/Service/GqlUserValidService.java
@@ -2,4 +2,23 @@ package com.IndieAn.GoFundIndie.Service;
 
 import com.IndieAn.GoFundIndie.Domain.Entity.User;
 import graphql.kickstart.servlet.context.GraphQLServletContext;
-import graphql.schema.DataFet
+import graphql.schema.DataFetchingEnvironment;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class GqlUserValidService {
+    private final UserService userService;
+
+    public int envValidCheck(DataFetchingEnvironment env) {
+        if(env == null) {
+            return 4000;
+        }
+
+        
