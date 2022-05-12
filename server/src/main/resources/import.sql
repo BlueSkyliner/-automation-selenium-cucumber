0cc8af36ba14d6712f55ec1ce3c338bac34d33f1
BlueSkyliner <96593707+BlueSkyliner@users.noreply.github.com>
2022-05-12 11:21:08 +0000

    Fixed #4

---
+++ server/src/main/resources/import.sql
@@ -0,0 +1,7 @@
+--Test Dummy Data
+--User
+INSERT INTO `user` (admin_role,banned,email,password,nickname) VALUES (true, false, "admin1@admin.com", "1234", "관리자 1호");
+INSERT INTO `user` (admin_role,banned,email,password,nickname) VALUES (true, false, "admin2@admin.com", "1234", "관리자 2호");
+
+INSERT INTO `user` (email,password,nickname,total_donation) VALUES ("test1@gofundindie.com", "1111", "김하나", 100);
+I
