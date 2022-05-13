54ceaf7eb0285bea960d1c9c424f3162b10de5c6
BlueSkyliner <96593707+BlueSkyliner@users.noreply.github.com>
2022-05-13 05:44:51 +0000

    Refactor utils

--- server/src/main/resources/import.sql
+++ server/src/main/resources/import.sql
@@ -4,4 +4,7 @@ INSERT INTO `user` (admin_role,banned,email,password,nickname) VALUES (true, fal
 INSERT INTO `user` (admin_role,banned,email,password,nickname) VALUES (true, false, "admin2@admin.com", "1234", "관리자 2호");
 
 INSERT INTO `user` (email,password,nickname,total_donation) VALUES ("test1@gofundindie.com", "1111", "김하나", 100);
-I
+INSERT INTO `user` (email,password,nickname,total_donation) VALUES ("test2@gofundindie.com", "2222", "이둘", 2000);
+INSERT INTO `user` (email,password,nickname,total_donation) VALUES ("test3@gofundindie.com", "3333", "박셋", 300);
+INSERT INTO `user` (email,password,nickname,total_donation) VALUES ("test4@gofundindie.com", "4444", "최넷", 0);
+INSERT INTO `user` (email,password,nickname,total_donation) VALUES ("test5@gofundindie.com", "5555", "
