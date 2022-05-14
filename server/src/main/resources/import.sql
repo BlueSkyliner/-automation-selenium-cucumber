1ce5f6f4b67c861a0d1d8167d2acff56864f6610
BlueSkyliner <96593707+BlueSkyliner@users.noreply.github.com>
2022-05-14 21:41:32 +0000

    Create sonar-project.properties

--- server/src/main/resources/import.sql
+++ server/src/main/resources/import.sql
@@ -7,4 +7,9 @@ INSERT INTO `user` (email,password,nickname,total_donation) VALUES ("test1@gofun
 INSERT INTO `user` (email,password,nickname,total_donation) VALUES ("test2@gofundindie.com", "2222", "이둘", 2000);
 INSERT INTO `user` (email,password,nickname,total_donation) VALUES ("test3@gofundindie.com", "3333", "박셋", 300);
 INSERT INTO `user` (email,password,nickname,total_donation) VALUES ("test4@gofundindie.com", "4444", "최넷", 0);
-INSERT INTO `user` (email,password,nickname,total_donation) VALUES ("test5@gofundindie.com", "5555", "
+INSERT INTO `user` (email,password,nickname,total_donation) VALUES ("test5@gofundindie.com", "5555", "한다섯", 0);
+INSERT INTO `user` (email,password,nickname,total_donation) VALUES ("askdjaskjd@gofundindie.com", "6666", "김여섯", 0);
+INSERT INTO `user` (email,password,nickname,total_donation) VALUES ("qwerasdf4567@gofundindie.com", "7777", "조일곱", 17000);
+INSERT INTO `user` (email,password,nickname,total_donation) VALUES ("mansoor@gofundindie.com", "8888", "만수르", 50000000);
+
+INSERT INTO `user` (email,password,nickname) VALUES ("mansoor@gofundindie.com"
