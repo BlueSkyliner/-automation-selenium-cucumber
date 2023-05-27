f6161b55b366ce749b7d3bf4bfa16987a64d924a
BlueSkyliner <96593707+BlueSkyliner@users.noreply.github.com>
2023-05-27 16:33:30 +0000

    update zh-Hant translation

---
+++ server/src/test/java/com/IndieAn/GoFundIndie/GqlTest.java
@@ -0,0 +1,6 @@
+package com.IndieAn.GoFundIndie;
+
+import com.graphql.spring.boot.test.GraphQLTestTemplate;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.facto
