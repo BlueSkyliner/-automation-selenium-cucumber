c798898791b1f2a279936040a3f839cf474e1c5f
BlueSkyliner <96593707+BlueSkyliner@users.noreply.github.com>
2023-05-31 09:33:31 +0000

    API doc fixes

--- server/src/test/java/com/IndieAn/GoFundIndie/TestDomain/UserTest/UserInfoTestDTO.java
+++ server/src/test/java/com/IndieAn/GoFundIndie/TestDomain/UserTest/UserInfoTestDTO.java
@@ -17,4 +17,24 @@ public class UserInfoTestDTO {
         this.id = id;
     }
 
-    public boolea
+    public boolean isAdmin_role() {
+        return admin_role;
+    }
+
+    public void setAdmin_role(boolean admin_role) {
+        this.admin_role = admin_role;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getProfile_picture() {
+        return profile_picture;
+    }
+
+    public void setProfile_picture(String profile_
